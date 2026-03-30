67efc087c2f10ba2e661b508513990b823f0b93d
declandfyne <declan@dfyne.com>
2026-03-30 17:45:43 +0100

    style: Add new product images and update collection data for improved product display

---
+++ src/data/collection.js
@@ -0,0 +1,55 @@
+import longlineStrappyIce from '../assets/collection/longline-strappy-ice.png'
+import longlineStrappyRaspberry from '../assets/collection/longline-strappy-raspberry.png'
+import strappyBraIce from '../assets/collection/strappy-bra-ice.png'
+import leggingsIce from '../assets/collection/leggings-ice.png'
+import strappyBraRaspberry from '../assets/collection/strappy-bra-raspberry.png'
+import leggingsRaspberry from '../assets/collection/leggings-raspberry.png'
+import shortSleeveSlateGrey from '../assets/collection/short-sleeve-slate-grey.png'
+import shortSleeveWinterBerry from '../assets/collection/short-sleeve-winter-berry.png'
+import shortSleeveEspresso from '../assets/collection/short-sleeve-espresso.png'
+import shortSleeveInk from '../assets/collection/short-sleeve-ink.png'
+import shortSleevePine from '../assets/collection/short-sleeve-pine.png'
+import shortsWinterBerry from '../assets/collection/shorts-winter-berry.png'
+import shortsEspresso from '../assets/collection/shorts-espresso.png'
+import shortsInk from '../assets/collection/shorts-ink.png'
+import shortsPine from '../assets/collection/shorts-pine.png'
+import strappyBraSlateGrey from '../assets/collection/strappy-bra-slate-grey.png'
+import strappyBraWinterBerry from '../assets/collection/strappy-bra-winter-berry.png'
+import strappyBraEspresso from '../assets/collection/strappy-bra-espresso.png'
+import strappyBraInk from '../assets/collection/strappy-bra-ink.png'
+import strappyBraPine from '../assets/collection/strappy-bra-pine.png'
+import leggingsWinterBerry from '../assets/collection/leggings-winter-berry.png'
+import leggingsEspresso from '../assets/collection/leggings-espresso.png'
+import leggingsInk from '../assets/collection/leggings-ink.png'
+import leggingsPine from '../assets/collection/leggings-pine.png'
+import cropZippySlateGrey from '../assets/collection/crop-zippy-slate-grey.png'
+import cropZippyWinterBerry from '../assets/collection/crop-zippy-winter-berry.png'
+
+export const COLLECTION_PRODUCTS = [
+  { id: 1,  name: 'Impact Longline Strappy Top', color: 'Ice',          price: '£44.99', reviews: 7508,  isNew: true,  img: longlineStrappyIce },
+  { id: 2,  name: 'Impact Longline Strappy Top', color: 'Raspberry',    price: '£44.99', reviews: 7508,  isNew: true,  img: longlineStrappyRaspberry },
+  { id: 3,  name: 'Impact Strappy Bra',          color: 'Ice',          price: '£33.99', reviews: 7198,  isNew: true,  img: strappyBraIce },
+  { id: 4,  name: 'Impact Leggings',             color: 'Ice',          price: '£50.99', reviews: 27762, isNew: true,  img: leggingsIce },
+  { id: 5,  name: 'Impact Strappy Bra',          color: 'Raspberry',    price: '£33.99', reviews: 7198,  isNew: true,  img: strappyBraRaspberry },
+  { id: 6,  name: 'Impact Leggings',             color: 'Raspberry',    price: '£50.99', reviews: 27762, isNew: true,  img: leggingsRaspberry },
+  { id: 7,  name: 'Impact Short Sleeve Top',     color: 'Slate Grey',   price: '£40.99', reviews: 4844,  isNew: true,  img: shortSleeveSlateGrey },
+  { id: 8,  name: 'Impact Short Sleeve Top',     color: 'Winter Berry', price: '£40.99', reviews: 4844,  isNew: false, img: shortSleeveWinterBerry },
+  { id: 9,  name: 'Impact Short Sleeve Top',     color: 'Espresso',     price: '£40.99', reviews: 4844,  isNew: false, img: shortSleeveEspresso },
+  { id: 10, name: 'Impact Short Sleeve Top',     color: 'Ink',          price: '£40.99', reviews: 4844,  isNew: false, img: shortSleeveInk },
+  { id: 11, name: 'Impact Short Sleeve Top',     color: 'Pine',         price: '£40.99', reviews: 4844,  isNew: false, img: shortSleevePine },
+  { id: 12, name: "Impact Shorts | 4.5\"",       color: 'Winter Berry', price: '£41.99', reviews: 61087, isNew: false, img: shortsWinterBerry },
+  { id: 13, name: "Impact Shorts | 4.5\"",       color: 'Espresso',     price: '£41.99', reviews: 61087, isNew: false, img: shortsEspresso },
+  { id: 14, name: "Impact Shorts | 4.5\"",       color: 'Ink',          price: '£41.99', reviews: 61087, isNew: false, img: shortsInk },
+  { id: 15, name: "Impact Shorts | 4.5\"",       color: 'Pine',         price: '£41.99', reviews: 61087, isNew: false, img: shortsPine },
+  { id: 16, name: 'Impact Strappy Bra',          color: 'Slate Grey',   price: '£33.99', reviews: 7198,  isNew: false, img: strappyBraSlateGrey },
+  { id: 17, name: 'Impact Strappy Bra',          color: 'Winter Berry', price: '£33.99', reviews: 7198,  isNew: false, img: strappyBraWinterBerry },
+  { id: 18, name: 'Impact Strappy Bra',          color: 'Espresso',     price: '£33.99', reviews: 7198,  isNew: false, img: strappyBraEspresso },
+  { id: 19, name: 'Impact Strappy Bra',          color: 'Ink',          price: '£33.99', reviews: 7198,  isNew: false, img: strappyBraInk },
+  { id: 20, name: 'Impact Strappy Bra',          color: 'Pine',         price: '£33.99', reviews: 7198,  isNew: false, img: strappyBraPine },
+  { id: 21, name: 'Impact Leggings',             color: 'Winter Berry', price: '£50.99', reviews: 27762, isNew: false, img: leggingsWinterBerry },
+  { id: 22, name: 'Impact Leggings',             color: 'Espresso',     price: '£50.99', reviews: 27762, isNew: false, img: leggingsEspresso },
+  { id: 23, name: 'Impact Leggings',             color: 'Ink',          price: '£50.99', reviews: 27762, isNew: false, img: leggingsInk },
+  { id: 24, name: 'Impact Leggings',             color: 'Pine',         price: '£50.99', reviews: 27762, isNew: false, img: leggingsPine },
+  { id: 25, name: 'Impact Crop Zippy',           color: 'Slate Grey',   price: '£55.99', reviews: 230,   isNew: false, img: cropZippySlateGrey },
+  { id: 26, name: 'Impact Crop Zippy',           color: 'Winter Berry', price: '£55.99', reviews: 230,   isNew: false, img: cropZippyWinterBerry },
+]
